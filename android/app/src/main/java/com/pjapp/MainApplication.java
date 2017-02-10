d2a3e4420bcdf7822b2b4619277f3d9d52391bee
vadim.ruban <vadim.ruban@carusto.com>
2017-02-10 12:38:46 +0200

    Upgrade to latest RN version

--- android/app/src/main/java/com/pjapp/MainApplication.java
+++ android/app/src/main/java/com/pjapp/MainApplication.java
@@ -4,12 +4,12 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.carusto.ReactNativePjSip.PjSipModulePackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
-import com.carusto.PjSipModulePackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -25,9 +25,9 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-        new MainReactPackage(),
-        new LinearGradientPackage(),
-        new PjSipModulePackage()
+          new MainReactPackage(),
+            new PjSipModulePackage(),
+            new LinearGradientPackage()
       );
     }
   };
