ab277c91b1872a70979d80b16118851d6287685a
Tariq Abusheikh <tariq.abusheikh@gmail.com>
2019-04-25 08:45:16 -0500

    Rename iOS app (2nd pass)

--- ios/RNSIPTests/RNSIPTests.m
+++ ios/RNSIPTests/RNSIPTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface PjAppTests : XCTestCase
+@interface RNSIPTests : XCTestCase
 
 @end
 
-@implementation PjAppTests
+@implementation RNSIPTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
